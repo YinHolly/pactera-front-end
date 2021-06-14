c0279dd8444e548c920f7f9c1f8d2ecc670f62d4
YinHolly <15998483623@163.com>
2021-06-15 00:42:58 +0800

    add build file

---
+++ build/precache-manifest.35070d4cca37ea4dfe6630fc2b42b2ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732cd3b4af2fda79c8b2c86b2fbb1687",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61087d4e1fdd51f26a19",
+    "url": "./static/js/2.f2f419d2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.f2f419d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afb8feb13ff86dc3f4e",
+    "url": "./static/js/main.f85fcb9a.chunk.js"
+  },
+  {
+    "revision": "a9c0b057581d654082f9",
+    "url": "./static/js/runtime-main.a9e8e296.js"
+  }
+]);
